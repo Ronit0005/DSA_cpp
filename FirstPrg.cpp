5f720ad612ce42995ff43e547f42de540e25ab9e
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-27 22:09:01 +0530

    Adding the first program half done to the repo

---
+++ FirstPrg.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //cout<<"This is my first c++ program"<<"\n";
+    //cout<<"Hello world, How are you";
+    //cout<<"Ronit\nKumar\nSingh";
+    //int age;
+    // cout<<"Input your age :\n";
+    // cin>>age;
+    // cout<<"Your age is : "<<age;
+    char chr='a';int age=1111;
+    cout<<"The character you entered is :"<<chr<<endl;
+    cout<<"The size of the character is :"<<sizeof(chr)<<endl;
+    cout<<"The sixe of the integer is :"<<sizeof(age);
+    return 0;
+}
+
+//Writing boilerplate code.
+/*#include <iostream>
+using namespace std;
+int main(){
+    
+}
+*/
