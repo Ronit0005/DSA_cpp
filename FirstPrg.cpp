df6c1fdb185a6780cd1c23fe595dcb75f45716f9
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-27 23:30:11 +0530

    editing the first prg file

--- FirstPrg.cpp
+++ FirstPrg.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 int main(){
     //cout<<"This is my first c++ program"<<"\n";
@@ -8,12 +8,23 @@ int main(){
     // cout<<"Input your age :\n";
     // cin>>age;
     // cout<<"Your age is : "<<age;
-    char chr='a';int age=1111;
-    cout<<"The character you entered is :"<<chr<<endl;
-    cout<<"The size of the character is :"<<sizeof(chr)<<endl;
-    cout<<"The sixe of the integer is :"<<sizeof(age);
+    // char chr='a';int age=1111;
+    // cout<<"The character you entered is :"<<chr<<endl;
+    // cout<<"The size of the character is :"<<sizeof(chr)<<endl;
+    // cout<<"The sixe of the integer is :"<<sizeof(age);
+    // Type Casting.
+    //float price=102.45f;
+    //double priced=price;
+    //cout<<"The price of the meal is :"<<priced;
+    // char chr='A';
+    // int chad=(int)chr;
+    // cout<<"The ASCII code for A is :"<<chad;
+    int a=11;int b=5;
+    cout<<"Sum is : "<<a+b<<endl;
+    cout<<"The division is : "<<a/(double)b;
     return 0;
 }
+*/
 
 //Writing boilerplate code.
 /*#include <iostream>
@@ -22,3 +33,15 @@ int main(){
     
 }
 */
+// Sum of two numbers.
+#include <iostream>
+using namespace std;
+int main(){
+    int inp1,inp2;
+    cout<<"Enter the first number :"<<endl;
+    cin>>inp1;
+    cout<<"Enter the second numbers :"<<endl;
+    cin>>inp2;
+    cout<<"The sum of the two numbers are : "<<(inp1+inp2);
+    return 0;
+}
