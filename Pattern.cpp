afc8d921beeef850fca5317e1200d54358ef14ac
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-29 13:30:08 +0530

    Adding the pattern file to the repo

---
+++ Pattern.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //PATTERN 1:
+    /*int i=1;
+    for(i=1;i<=9;i++){
+        cout<<i;
+        if(i%3==0){
+            cout<<endl;
+        }
+    }
+    */
+   // PATTERN 2:
+    /*for(int i=1;i<=5;i++){
+    for(int j=1;j<=i;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+   }
+   */
+    
+    return 0;
+}
