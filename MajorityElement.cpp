27eca0b06d80911f2538882a02e2a26aa2892fb8
Ronti0005 <rontisingh24012007@gmail.com>
2025-02-08 22:00:42 +0530

    Editing the Majority element file

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,18 +1,45 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
-#include <vector>
 int main(){
-    vector<int> arr={1,2,2,1,1};
-    vector<int> name;
-    vector<int> frequency;
-    int count=0;
-    int n=sizeof(arr)/sizeof(int);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(arr[i]==arr[j]&&i!=j){
-               count++;
-               arr.
+    int arr[]={1,2,2,1,1};
+    int freq=0;
+    int size=sizeof(arr)/sizeof(int);
+    int i;
+    for(i=0;i<size;i++){
+        freq=0;
+        for(int j=0;j<size;j++){
+            if(i!=j&&arr[i]==arr[j]){
+              freq++;
+              }
+            }
+            if(freq>=size/2){
+                cout<<"Majority element is : "<<arr[i];
+                break;
             }
         }
+    return 0;
+}
+    */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int freq=1;
+    vector<int> vec={1,2,2,1,1};
+    sort(vec.begin(),vec.end());
+    for(int i=0;i<vec.size();i++){
+       if(vec[i]==vec[i+1]){
+        freq++;
+       }
+       else{
+        freq=1;
+       }
+       if(freq>=vec.size()/2){
+        cout<<"Majority Element is "<<vec[i];
+        break;
+       }
     }
+    return 0;
 }
