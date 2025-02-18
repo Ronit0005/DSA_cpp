62c0683ae606d5d0b8606a659ae096d50026ff0e
Ronti0005 <rontisingh24012007@gmail.com>
2025-02-18 22:31:54 +0530

    Adding dsa files to the repo

--- SearchInTwoDArray.cpp
+++ SearchInTwoDArray.cpp
@@ -17,7 +17,7 @@ int main(){
 
 // MY APPROCH:
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 int main(){
     int arr[3][4]={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
@@ -33,6 +33,36 @@ int main(){
                 }
             }
             break;
+        }
+    }
+    return 0;
+}*/
+
+
+
+// TYPE 2:
+
+#include <iostream>
+using namespace std;
+#include <vector>
+int main(){
+    vector <vector<int>> vec={{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,22},{18,21,23,26,29}};
+    int target=5;
+    int row=5;
+    int col=5;
+    int mid=vec[0][col-1];
+    int r=0;
+    int c=4;
+    while(r<5&&c>=0){
+        if(vec[r][c]==target){
+            cout<<"The element is found at the index : "<<r<<" , "<<c;
+            break;
+        }
+        else if(vec[r][c]>target){
+            c--;
+        }
+        else {
+            r++;
         }
     }
     return 0;
